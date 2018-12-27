4c681e99dbe65d0e6b39341d30ca89325de6c76c
XY-ZHOU <731986251@qq.com>
2018-12-27 21:15:20 +0800

    code format

--- whack_a_mole/js/game-easy.js
+++ whack_a_mole/js/game-easy.js
@@ -34,18 +34,14 @@ window.onload = function() {
       clearTimeout(moleStay);
     }, gameTime)
   }
-  /**
-   * 初始化设置.
-   */
+
   function resetScoreAndTime() {
     titleH1.innerText = 'WHACK-A-MOLE!';
     timeUp = false;
     score = 0;
     ScoreBoard.innerText = score;
   }
-  /**
-   * 出洞.
-   */
+
   function peep() {
     if (!timeUp) {
       clearTimeout(moleStay);
@@ -54,22 +50,11 @@ window.onload = function() {
       comeOutAndStop(MOLE, TIME);
     }
   }
-  /**
-   * 随机生成地鼠出洞的停留时间. 该时间其实是[min, max]间的随机数.
-   *
-   * @param min 随机数的下界.
-   * @param max 随机数的上界.
-   * @returns {number}
-   */
+
   function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
   }
-  /**
-   * 随机选择地鼠钻出的地洞，如果与上一个是相同地洞，则重新选择一个地洞.
-   *
-   * @param holes
-   * @returns {*}
-   */
+
   function getRandomMole(Moles) {
     let newMole = getRandom(1, 6);
     while (newMole === lastMole) {
@@ -78,12 +63,7 @@ window.onload = function() {
     lastMole = lastMole;
     return Moles[newMole];
   }
-  /**
-   * 地鼠出洞并停留相应时间，如果游戏时间未结束(timeUp)，继续出洞(peep).
-   *
-   * @param hole 地鼠所出地洞.
-   * @param time 地鼠停留时间.
-   */
+
   function comeOutAndStop(mole, time) {
     if (!timeUp) {
       mole.classList.add('up');
@@ -93,9 +73,6 @@ window.onload = function() {
       }, time);
     }
   }
-  /**
-   * 打地鼠。为每个MOLES添加点击事件，点击后分数显示+1，地鼠入洞。
-   */
   Moles.forEach(MOLE => MOLE.addEventListener('click', function(e) {
     MOLE.classList.remove('up');
     score += 1;
