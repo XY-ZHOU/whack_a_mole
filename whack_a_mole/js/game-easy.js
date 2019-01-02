87ed0700a176c9435c3c0fdf86a2db98e2d18c02
XY-ZHOU <731986251@qq.com>
2019-01-02 20:17:12 +0800

    modify code after review

--- whack_a_mole/js/game-easy.js
+++ whack_a_mole/js/game-easy.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-  const SCOREBORAD = document.querySelector('.score');
+  const SCOREBOARD = document.querySelector('.score');
   const MOLES = document.querySelectorAll('.mole');
   const STARTBTN = document.getElementById('start_btn');
   let titleH1 = document.getElementById('title');
-  let lastMole;
   let timeUp = false;
   let score = 0;
   let gameTime = 10000;
-  let moleStay;
+  let time;
+  let mole;
   STARTBTN.addEventListener('click', function() {
     showBtnAnimation();
     startGame();
@@ -18,7 +18,7 @@ window.onload = function() {
     STARTBTN.classList.add('animate');
     setTimeout(() => {
       STARTBTN.classList.remove('animate');
-      STARTBTN.style.display = 'none';
+      STARTBTN.classList.add('disappear');
     }, 700);
   }
 
@@ -26,57 +26,61 @@ window.onload = function() {
     resetScoreAndTime();
     peep();
     setTimeout(() => {
-      titleH1.innerText = 'TIME UP!';
-      ScoreBoard.innerText = score;
-      StartBtn.innerText = 'Replay';
-      StartBtn.style.display = 'inline-block';
+      showTxt(titleH1, 'TIME UP!');
+      showScore(SCOREBOARD, score);
+      STARTBTN.innerText = 'Replay';
+      STARTBTN.classList.remove('disappear');
       timeUp = true;
-      clearTimeout(moleStay);
-    }, gameTime)
+    }, gameTime);
   }
 
   function resetScoreAndTime() {
-    titleH1.innerText = 'WHACK-A-MOLE!';
+    showTxt(titleH1, 'WHACK-A-MOLE!');
     timeUp = false;
     score = 0;
-    ScoreBoard.innerText = score;
+    showScore(SCOREBOARD, score);
   }
 
   function peep() {
-    if (!timeUp) {
-      clearTimeout(moleStay);
-      const TIME = getRandom(500, 1000);
-      const MOLE = getRandomMole(Moles);
-      comeOutAndStop(MOLE, TIME);
-    }
-  }
-
-  function getRandom(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-
-  function getRandomMole(Moles) {
-    let newMole = getRandom(1, 6);
-    while (newMole === lastMole) {
-      lastMole = getRandom(1, 6);
-    }
-    lastMole = lastMole;
-    return Moles[newMole];
+    time = getRandom(500, 1000);
+    mole = getRandomMole(MOLES);
+    comeOutAndStop(mole, time);
   }
 
   function comeOutAndStop(mole, time) {
-    if (!timeUp) {
-      mole.classList.add('up');
-      moleStay = setTimeout(function() {
-        mole.classList.remove('up');
+    mole.classList.add('up');
+    setTimeout(() => {
+      mole.classList.remove('up');
+      if (!timeUp) {
         peep();
-      }, time);
-    }
+      }
+    }, time);
   }
-  Moles.forEach(MOLE => MOLE.addEventListener('click', function(e) {
-    MOLE.classList.remove('up');
+  MOLES.forEach(mole => mole.addEventListener('click', function(e) {
     score += 1;
-    ScoreBoard.innerText = score;
-    peep();
+    showScore(SCOREBOARD, score);
+    mole.classList.remove('up');
   }));
 };
+
+function getRandomMole(MOLES) {
+  let lastMole;
+  let newMole = getRandom(1, 6);
+  while (newMole === lastMole) {
+    newMole = getRandom(1, 6);
+  }
+  lastMole = newMole;
+  return MOLES[newMole];
+}
+
+function showTxt(titleH1, txt) {
+  titleH1.innerText = txt;
+}
+
+function showScore(SCOREBOARD, score) {
+  SCOREBOARD.innerText = score;
+}
+
+function getRandom(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
